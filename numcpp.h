1846e9bc6ae28c4b69765227c1bf259edc998190
Victor Daniel Alvarado <vdae_2304@hotmail.com>
2022-08-03 15:50:37 -0500

    Update numcpp.h

--- numcpp.h
+++ numcpp.h
@@ -1,13 +1,14 @@
 /*
  * This file is part of the NumCpp project.
  *
- * NumCpp is a package for scientific computing in C++. It is a C++ library
- * that provides an array and a matrix object, and an assortment of routines
- * for fast operations on arrays and matrices, including mathematical, logical,
+ * NumCPP is a package for scientific computing in C++. It is a C++ library
+ * that provides support for multidimensional arrays, and defines an assortment
+ * of routines for fast operations on them, including mathematical, logical,
  * sorting, selecting, I/O and much more.
  *
- * The NumCpp package is inspired by the NumPy package for Python, although it
- * is not related to it or any of its parts.
+ * NumCPP comes from Numeric C++ and, as the name suggests, is a package
+ * inspired by the NumPy package for Python, although it is completely
+ * independent from its Python counterpart.
  *
  * This program is free software: you can redistribute it and/or modify it by
  * giving enough credit to its creators.
@@ -22,12 +23,31 @@
 #ifndef NUMCPP_H_INCLUDED
 #define NUMCPP_H_INCLUDED
 
-#include "numcpp/array.h"
-#include "numcpp/matrix.h"
+#include "numcpp/tensor.h"
 #include "numcpp/math.h"
-#include "numcpp/routines.h"
-#include "numcpp/broadcasting.h"
+// #include "numcpp/routines.h"
+// #include "numcpp/broadcasting.h"
 #include "numcpp/random.h"
 #include "numcpp/io.h"
+
+namespace numcpp {
+    template <class T>
+    using array = tensor<T, 1>;
+
+    template <class T>
+    using array_view = tensor_view<T, 1>;
+
+    template <class T>
+    using indirect_array = indirect_tensor<T, 1>;
+
+    template <class T>
+    using matrix = tensor<T, 2>;
+
+    template <class T>
+    using matrix_view = tensor_view<T, 2>;
+
+    template <class T>
+    using indirect_matrix = indirect_tensor<T, 2>;
+}
 
 #endif // NUMCPP_H_INCLUDED
